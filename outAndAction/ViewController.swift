ac352126540e9892b185ce34ede50216ccd018e9
iteaky <iteaky@gmail.com>
2020-04-12 17:46:09 +0300

    Initial Commit

---
+++ outAndAction/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  outAndAction
+//
+//  Created by Костя on 12.04.2020.
+//  Copyright © 2020 Konstantin Evstafev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
